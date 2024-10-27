733c03100fe5336a940b0ac77a6f8e7461e0b2d1
tecnology-expart <mdhakimshorkar123@gmail.com>
2024-10-27 21:08:17 +0600

    milestone-08-test

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+      <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+    <div>
+      
+    </div>
+
+
+
+
+
+      </div>
+
+
+      <div>
+      <div className="diff aspect-[16/9]">
+  <div className="diff-item-1">
+    <img alt="daisy" src="https://img.daisyui.com/images/stock/photo-1560717789-0ac7c58ac90a.webp" />
+  </div>
+  <div className="diff-item-2">
+    <img
+      alt="daisy"
+      src="https://img.daisyui.com/images/stock/photo-1560717789-0ac7c58ac90a-blur.webp" />
+  </div>
+  <div className="diff-resizer"></div>
+</div>
+      </div>
+    </>
+  )
+}
+
+export default App
